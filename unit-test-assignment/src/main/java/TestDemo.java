a62ed91a5696e2917378db92c5b77ccd568316dd
Nicholas DeSimone <nckdsmn@yahoo.com>
2022-09-17 23:31:51 -0400

    Add files via upload

--- unit-test-assignment/src/main/java/TestDemo.java
+++ unit-test-assignment/src/main/java/TestDemo.java
@@ -1,23 +1,22 @@
 import java.util.Random;
 
 public class TestDemo {
-
-	public int addPositive(int a, int b) {
-		if(a <= 0 || b <= 0) {
-			throw new IllegalArgumentException("Both parameters must be positive!");
-		} 
-		return a + b;
-		
-	}
 	
-	public int randomNumberSquared() {
-		int randomInt = getRandomInt();
-		return randomInt * randomInt;
+	public int addPositive(int a, int b) {
+		if (a <= 0 || b <= 0) {
+			throw new IllegalArgumentException("Both parameters must be positive!");
+		} else
+			return a + b;
+
+	}
+
+	int randomNumberSquared() {
+		int a = getRandomInt();
+		return a * a;
 	}
 
 	int getRandomInt() {
 		Random random = new Random();
 		return random.nextInt(10) + 1;
-		}
+	}
 }
-
--- unit-test-assignment/src/test/java/TestDemoTest.java
+++ unit-test-assignment/src/test/java/TestDemoTest.java
@@ -1,23 +1,20 @@
 import java.util.stream.Stream;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
 
 import org.junit.jupiter.params.provider.MethodSource;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.doReturn;
 
 class TestDemoTest {
 
 	private TestDemo testDemo;
-	TestDemo mockDemo = spy(testDemo);
-	
+		
 	@BeforeEach
 	void setUp() throws Exception {
 		testDemo = new TestDemo();
@@ -34,17 +31,23 @@ class TestDemoTest {
 		}
 	}
 	
-	@Test
-	void assertThatNumberSquaredIsCorrect() {
-		doReturn(5).when(mockDemo).getRandomInt();
-		int fiveSquared = mockDemo.randomNumberSquared();
-		assertThat(fiveSquared).isEqualTo(25);
-	}
+//	@Test
+//	void assertThatNumberSquaredIsCorrect() {
+//		
+//		
+//		TestDemo mockDemo = spy(testDemo);
+//		doReturn(5).when(mockDemo).getRandomInt();
+//		
+//		int fiveSquared = mockDemo.randomNumberSquared();
+//		
+//		assertThat(fiveSquared).isEqualTo(25);
+//	}
 	
 	static Stream<Arguments> argumentsForAddPositive(){
 		return Stream.of(
 				arguments(2, 4, 6, false),
-				arguments(0, 2, 2, true)
+				arguments(0, 2, 2, true),
+				arguments(-1, -1, -2, true)
 				);
 	}
 
